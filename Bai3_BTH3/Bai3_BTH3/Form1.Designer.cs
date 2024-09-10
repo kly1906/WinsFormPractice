7a13a4063c2311281af1dd17b90574cb30fdf6fc
Hoàng Thị Minh Tâm <162414914+kly1906@users.noreply.github.com>
2024-09-10 19:08:53 +0700

    Add files via upload

---
+++ Bai3_BTH3/Bai3_BTH3/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Bai3_BTH3
+{
+    partial class frmcbb
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            grbInput = new GroupBox();
+            cbbUpdate = new ComboBox();
+            btnUpdate = new Button();
+            txtInput = new TextBox();
+            groupBox2 = new GroupBox();
+            lstDivisor = new ListBox();
+            btnSum = new Button();
+            btntEven = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            grbInput.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grbInput
+            // 
+            grbInput.Controls.Add(cbbUpdate);
+            grbInput.Controls.Add(btnUpdate);
+            grbInput.Controls.Add(txtInput);
+            grbInput.Location = new Point(38, 12);
+            grbInput.Name = "grbInput";
+            grbInput.Size = new Size(273, 133);
+            grbInput.TabIndex = 0;
+            grbInput.TabStop = false;
+            grbInput.Text = "Nhập số";
+            // 
+            // cbbUpdate
+            // 
+            cbbUpdate.FormattingEnabled = true;
+            cbbUpdate.Location = new Point(16, 78);
+            cbbUpdate.Name = "cbbUpdate";
+            cbbUpdate.Size = new Size(236, 29);
+            cbbUpdate.TabIndex = 2;
+            cbbUpdate.SelectedIndexChanged += cbbUpdate_SelectedIndexChanged;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(171, 22);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(81, 39);
+            btnUpdate.TabIndex = 1;
+            btnUpdate.Text = "&Cập nhật";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(16, 28);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(149, 29);
+            txtInput.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(lstDivisor);
+            groupBox2.Location = new Point(365, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(212, 133);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Danh sách các ước số";
+            // 
+            // lstDivisor
+            // 
+            lstDivisor.FormattingEnabled = true;
+            lstDivisor.ItemHeight = 21;
+            lstDivisor.Location = new Point(17, 28);
+            lstDivisor.Name = "lstDivisor";
+            lstDivisor.ScrollAlwaysVisible = true;
+            lstDivisor.Size = new Size(179, 88);
+            lstDivisor.TabIndex = 3;
+            // 
+            // btnSum
+            // 
+            btnSum.Location = new Point(365, 151);
+            btnSum.Name = "btnSum";
+            btnSum.Size = new Size(212, 39);
+            btnSum.TabIndex = 4;
+            btnSum.Text = "&Tổng các ước số";
+            btnSum.UseVisualStyleBackColor = true;
+            btnSum.Click += btnSum_Click;
+            // 
+            // btntEven
+            // 
+            btntEven.Location = new Point(365, 196);
+            btntEven.Name = "btntEven";
+            btntEven.Size = new Size(212, 39);
+            btntEven.TabIndex = 5;
+            btntEven.Text = "&Số lượng ước số chẵn";
+            btntEven.UseVisualStyleBackColor = true;
+            btntEven.Click += btntEven_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(365, 241);
+            button4.Name = "button4";
+            button4.Size = new Size(212, 39);
+            button4.TabIndex = 6;
+            button4.Text = "&Số lượng ước số nguyên tố";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(226, 241);
+            button5.Name = "button5";
+            button5.Size = new Size(101, 39);
+            button5.TabIndex = 7;
+            button5.Text = "&Thoát";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // frmcbb
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(614, 292);
+            Controls.Add(groupBox2);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(btntEven);
+            Controls.Add(btnSum);
+            Controls.Add(grbInput);
+            Name = "frmcbb";
+            Text = "Combobox";
+            grbInput.ResumeLayout(false);
+            grbInput.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox grbInput;
+        private ComboBox cbbUpdate;
+        private Button btnUpdate;
+        private TextBox txtInput;
+        private GroupBox groupBox2;
+        private ListBox lstDivisor;
+        private Button btnSum;
+        private Button btntEven;
+        private Button button4;
+        private Button button5;
+    }
+}
---
+++ Bai3_BTH3/Bai3_BTH3/Form1.cs
@@ -0,0 +1,169 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Bai3_BTH3
+{
+    public partial class frmcbb : Form
+    {
+        public frmcbb()
+        {
+            InitializeComponent();
+        }
+
+        //sự kiện click của button cập nhật
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //nếu textbox trống, đặt con trỏ vào textbox
+            if (this.txtInput.Text.Length == 0)
+            {
+                txtInput.Focus();
+                return; //kết thúc sự kiện click
+            }
+
+            int number;
+            if (int.TryParse(txtInput.Text, out number))
+            {
+                //nếu nội dung textbox là số nguyên
+                //hiển thị số mới nhập trên thanh combobox, thêm vào vị trí đầu tiên 
+                cbbUpdate.Text = txtInput.Text;
+                cbbUpdate.Items.Insert(0, txtInput.Text);
+                UpdateDivisorsList();//hiển thị danh sách ước của số nguyên vừa nhập
+                //xóa nội dung textbox vừa nhập và đặt con trỏ trở lại textbox
+                txtInput.Clear();
+                txtInput.Focus();
+            }
+            else
+            {
+                //nếu không phải số nguyên, hiển thị thông báo 
+                MessageBox.Show("Vui lòng nhập một số!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtInput.Clear();
+                txtInput.Focus();
+            }
+
+        }
+
+        private void cbbUpdate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateDivisorsList(); //hiển thị ước của số nguyên được chọn
+        }
+
+        //phương thức tính các ước của số nguyên
+        private List<int> GetDivisors(int number)
+        {
+            List<int> divisors = new List<int>();
+
+            // Tính toán các ước số của số nguyên
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    divisors.Add(i);
+                }
+            }
+
+            return divisors;
+        }
+
+        //phương thức hiển thị các ước ra listbox
+        private void UpdateDivisorsList()
+        {
+            int selectedNumber = int.Parse(cbbUpdate.Text);
+
+            //tính toán các ước của số nguyên
+            List<int> divisors = GetDivisors(selectedNumber);
+            //xóa nội dung cũ và hiện thị nội dung mới ra listbox
+            lstDivisor.Items.Clear();
+            foreach (int divisor in divisors)
+            {
+                lstDivisor.Items.Add(divisor);
+            }
+        }
+
+        //tính tổng các ước 
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            if (lstDivisor.Items.Count > 0)
+            {
+                foreach (int divisor in lstDivisor.Items)
+                {
+                    sum += divisor;
+                }
+                MessageBox.Show($"Tổng các ước: {sum}", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                MessageBox.Show("Danh sách trống!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        //đếm số ước chẵn
+        private void btntEven_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            if (lstDivisor.Items.Count > 0)
+            {
+                foreach (int divisor in lstDivisor.Items)
+                {
+                    if (divisor % 2 == 0)
+                    {
+                        count += divisor;
+                    }
+                }
+                MessageBox.Show($"Tổng các ước: {count}", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                MessageBox.Show("Danh sách trống!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        //đếm số ước nguyên tố
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            bool check = false;
+            if (lstDivisor.Items.Count > 0)
+            {
+                foreach (int divisor in lstDivisor.Items)
+                { 
+                    if (IsPrime(divisor) == true)
+                    {
+                        count++;
+                    }
+                }
+
+                MessageBox.Show($"Tổng các ước: {count}", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                MessageBox.Show("Danh sách trống!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        //phương thức kiểm tra số nguyên tố
+        private bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false; //Số nhỏ hơn hoặc bằng 1 không phải là số nguyên tố
+
+            if (number == 2)
+                return true; //2 là số nguyên tố
+
+            if (number % 2 == 0)
+                return false; //Các số chẵn khác 2 không phải là số nguyên tố
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0)
+                    return false; //Nếu chia hết cho số lẻ khác 1 thì không phải số nguyên tố
+            }
+
+            return true; //Số không chia hết cho bất kỳ số nào khác ngoài 1 và chính nó là số nguyên tố
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
